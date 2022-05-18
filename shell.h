e3da9b890765943b6449ea9b35e26d1ed9b8ea5b
Dikachis <dikachi015safety@gmail.com>
2022-05-18 08:34:09 +0000

    Betty checks

---
+++ shell.h
@@ -0,0 +1,19 @@
+#ifndef _SHELL_H_
+#define _SHELL_H_
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/wait.h>
+#include <limits.h>
+#include <signal.h>
+
+
+void _env(char **com)
+
+
+
+#endif /* _SHELL_H_ */
